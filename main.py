1a175f1a37006b7758a7ad3ff6197c01a72ee7d4
Harry <hazzham@gmail.com>
2023-03-29 17:50:43 +0100

    Create function to parse through the JSON data

--- main.py
+++ main.py
@@ -12,13 +12,28 @@ def make_call(site_id, call):
     return api_response
 
 def parse_data(api_response):
-    """We need to parse through the data 10 times.
-    We get five days worth of data, each day giving us two things to look at (day n nite)"""
+    """We need to parse through the data 8 times.
+    We want 4 days worth of data (inc today), each day giving us two things to look at (day n nite)"""
+
+    weather_types = []  # list containing all the weather types for the next 72 hours
+    rain_probabilities = []  # list containing % chance of rain for the next 72 hours
+    for counter in range(4):
+        day_weather_type = json.dumps(api_response["SiteRep"]["DV"]["Location"]["Period"][counter]["Rep"][0]["W"])
+        night_weather_type = json.dumps(api_response["SiteRep"]["DV"]["Location"]["Period"][counter]["Rep"][1]["W"])
+
+        day_rain_probability = json.dumps(api_response["SiteRep"]["DV"]["Location"]["Period"][counter]["Rep"][0]["Pp"])
+        night_rain_probability = json.dumps(api_response["SiteRep"]["DV"]["Location"]["Period"][counter]["Rep"][1]["Pp"])
+
+        weather_types.append(day_weather_type)
+        weather_types.append(night_weather_type)
+        rain_probabilities.append(day_rain_probability)
+        rain_probabilities.append(night_rain_probability)
+
+    return weather_types, rain_probabilities
 
 
 def is_weather_acceptable():
     """ decides if weather is acceptable by looking at weather type and Pp% """
-    continue
 
 def main():
     """this calls all of the functions.  if is_weather_acceptable returns T, send email"""
